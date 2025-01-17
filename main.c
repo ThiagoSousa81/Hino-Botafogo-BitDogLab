a1b82c47d770f41493f88891a8f4cbbc3c65f0c7
El-Tomaz <tomazlemosgarcia@gmail.com>
2025-01-16 21:04:14 -0300

    Funcionaaaa!!!! Falta: - Organizar codebase - Escrever README

--- main.c
+++ main.c
@@ -3,27 +3,37 @@
  * 06/12/2024
  */
 
+// dependencias comuns
+
+// dependencias buzzer
 #include "pico/stdlib.h"
 #include "hardware/pwm.h"
 #include "hardware/clocks.h"
 #include "hardware/gpio.h"
-#include "hardware/adc.h"
-
 #include "inc/musica.h"
 
+// dependencias display
 #include "inc/ssd1306.h"
-#include "inc/bitmap_botafogo.h"    
+#include "inc/bitmap_botafogo.h"
 #include <string.h>
-// notas musicais
 
+// dependencias leds
+#include "hardware/pio.h"
+#include "inc/neopixel.h"
 // Mapeando hardware
 #define BUZZER_PIN 21
 #define BUTTON_A_PIN 6
 
 const uint I2C_SDA = 14;
 const uint I2C_SCL = 15;
+
 // Configuração da frequência do buzzer (em Hz)
 #define top 65535
+
+//configuracoes
+#define VOLUME_BUZZER
+#define BRILHO_LEDS
+
 
 // Definição de uma função para inicializar o PWM no pino do buzzer
 void pwm_init_buzzer(uint pin, uint slice_num)
@@ -63,7 +73,8 @@ void beep(uint pin, uint duration_ms)
 }
 
 // funcao para gravar os tempos de cada nota (e o espaço entre elas também)
-void gravar(int* tempos_off,int* tempos_on, uint slice_num){
+void gravar(int *tempos_off, int *tempos_on, uint slice_num)
+{
     uint32_t t1 = 0;
     uint32_t t0 = 0;
     for (int count = 0; count < 74; count++)
@@ -93,7 +104,6 @@ void gravar(int* tempos_off,int* tempos_on, uint slice_num){
         sleep_ms(100);
     }
 
-
     // Printando dados na serial se quiser reutilizar vvvvvvvvv
     printf("const int tempos_off[] = {\n");
     for (int i = 0; i < 74; i++)
@@ -108,14 +118,53 @@ void gravar(int* tempos_off,int* tempos_on, uint slice_num){
         printf("%d,\n", tempos_on[i]);
     }
     printf("}\n");
-
 }
 
+const uint heart2[] = {
+    2,
+    6, 8,
+    10, 14,
+    15, 17, 19,
+    21, 23};
+bool state = true;
+bool dim_callback(__unused struct repeating_timer *t)
+{
+
+    printf("oieee");
+    if (state == true)
+    {
+
+        for (int i = 0; i < 10; i++)
+        {
+            npSetLED(heart2[i], 10, 0, 0);
+        }
+        npWrite();
+        state = false;
+        printf("ligando");
+    }
+    else
+    {
+        npClear();
+        npWrite();
+        state = true;
+        printf("desligandooo");
+    }
+
+    return true;
+}
 
 int main()
 {
     // Inicializar o sistema de saída padrão
     stdio_init_all();
+
+    // Inicializa matriz de LEDs NeoPixel.
+    npInit(LED_PIN);
+    npClear();
+
+    // inicializando piscada do led
+    struct repeating_timer timer;
+    add_repeating_timer_ms(500, dim_callback, NULL, &timer);
 
 
     // Inicialização do i2c
@@ -143,10 +192,6 @@ int main()
     memset(ssd, 0, ssd1306_buffer_length);
     render_on_display(ssd, &frame_area);
 
-
-
-
-
     // Inicializar o PWM no pino do buzzer
     uint slice_num = pwm_gpio_to_slice_num(BUZZER_PIN);
     pwm_init_buzzer(BUZZER_PIN, slice_num);
@@ -156,8 +201,6 @@ int main()
     gpio_init(BUTTON_A_PIN);
     gpio_set_dir(BUTTON_A_PIN, GPIO_IN);
     gpio_pull_up(BUTTON_A_PIN);
-   
-    
 
     // desenhando escudo botafogo:
     ssd1306_t ssd_bm;
@@ -166,11 +209,11 @@ int main()
 
     ssd1306_draw_bitmap(&ssd_bm, bitmap_botafogo);
 
-    sleep_ms(1000); //pausinha dramática
+    sleep_ms(1000); // pausinha dramática
 
     for (int i = 0; i < 74; i++)
     {
-        pwm_set_gpio_level(BUZZER_PIN, 500);
+        pwm_set_gpio_level(BUZZER_PIN, VOLUME_BUZZER);
         change_note(musica[i]);
         sleep_us(tempos_on[i]);
 
@@ -179,7 +222,6 @@ int main()
     }
     pwm_set_gpio_level(BUZZER_PIN, 0);
 
-   
     while (true)
         ;
     return 0;
